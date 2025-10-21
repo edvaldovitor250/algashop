18080313ef8f1c4e13b047fb103d635c78dd2a07
Edvaldo Vitor <edvaldovitor250@gmail.com>
2025-10-20 21:08:29 -0300

    Contract and Listing Appeal

--- microservices/product-catalog/src/main/java/com/algaworks/algashop/product_catalog/presentation/ProductController.java
+++ microservices/product-catalog/src/main/java/com/algaworks/algashop/product_catalog/presentation/ProductController.java
@@ -1,12 +1,14 @@
-package com.algaworks.algashop.product_catalog.presentation;
+package com.algaworks.algashop.product.catalog.presentation;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
-
-import com.algaworks.algashop.product.catalog.presentation.ProductDetailOutput;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -14,11 +16,13 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ProductDetailOutput create(@RequestBody ProductInput input) {
+    public ProductDetailOutput create(@RequestBody @Valid ProductInput input) {
         return ProductDetailOutput.builder()
                 .id(UUID.randomUUID())
                 .addedAt(OffsetDateTime.now())
                 .inStock(false)
+                .name(input.getName())
+                .brand(input.getBrand())
                 .name(input.getName())
                 .brand(input.getBrand())
                 .description(input.getDescription())
@@ -52,4 +56,52 @@ public class ProductController {
                         .build())
                 .build();
     }
+
+    @GetMapping
+    public PageModel<ProductDetailOutput> filter(
+            @RequestParam(name = "size", required = false) Integer size,
+            @RequestParam(name = "number", required = false) Integer number
+    ) {
+        return PageModel.<ProductDetailOutput>builder()
+                .number(0)
+                .size(size)
+                .totalPages(1)
+                .totalElements(2)
+                .content(
+                        List.of(
+                                ProductDetailOutput.builder()
+                                        .id(UUID.randomUUID())
+                                        .addedAt(OffsetDateTime.now())
+                                        .name("Notebook X11")
+                                        .brand("Deep Diver")
+                                        .description("A Gamer Notebook")
+                                        .regularPrice(new BigDecimal("1500.00"))
+                                        .salePrice(new BigDecimal("1000.00"))
+                                        .inStock(true)
+                                        .enabled(true)
+                                        .category(CategoryMinimalOutput.builder()
+                                                .id(UUID.randomUUID())
+                                                .name("Notebook")
+                                                .build())
+                                        .build(),
+                                ProductDetailOutput.builder()
+                                        .id(UUID.randomUUID())
+                                        .addedAt(OffsetDateTime.now())
+                                        .name("Desktop I9000")
+                                        .brand("Deep Diver")
+                                        .description("A Gamer Desktop")
+                                        .regularPrice(new BigDecimal("3500.00"))
+                                        .salePrice(new BigDecimal("3000.00"))
+                                        .inStock(false)
+                                        .enabled(true)
+                                        .category(CategoryMinimalOutput.builder()
+                                                .id(UUID.randomUUID())
+                                                .name("Desktop")
+                                                .build())
+                                        .build()
+                        )
+                )
+                .build();
+    }
+
 }
